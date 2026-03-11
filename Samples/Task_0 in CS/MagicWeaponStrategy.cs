e97b95b938c0b8de52a0f0065be7ea551669a2a1
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2026-03-11 18:48:22 +0700

    Add files via upload

---
+++ Samples/Task_0 in CS/MagicWeaponStrategy.cs
@@ -0,0 +1,10 @@
+﻿namespace Task_0
+{
+    internal class MagicWeaponStrategy : IWeaponStrategy
+    {
+        public void Attack()
+        {
+            Console.WriteLine("Magic Weapon atatck!");
+        }
+    }
+}
---
+++ Samples/Task_0 in CS/Player.cs
@@ -0,0 +1,42 @@
+﻿namespace Task_0
+{
+    internal class Player
+    {
+        private IWeaponStrategy _weaponStrategy;
+
+        // Анализ проблемы: 
+        // Нарушение открытости-закрытости
+        // Для добавление нового оружия, нужно переписать код 
+        // Длинная лестница if-else
+
+        // Было так:
+        //public void Attack(string weapon_type)
+        //{
+        //    if (weapon_type == "Sword")
+        //        Console.WriteLine("Sword Attack!");
+
+        //    if (weapon_type == "Bow")
+        //        Console.WriteLine("Bow Attack!");
+
+        //    if (weapon_type == "Magic")
+        //        Console.WriteLine("Magic Attack!");
+        //}
+
+        // Стало: 
+        public void SetWeaponStrategy(IWeaponStrategy weaponStrategy)
+        {
+            _weaponStrategy = weaponStrategy;
+        }
+
+        public void Attack()
+        {
+            if (_weaponStrategy == null)
+            {
+                Console.WriteLine("WeaponStrategy is NULL!");
+                return;
+            }
+
+            _weaponStrategy.Attack();
+        }
+    }
+}
